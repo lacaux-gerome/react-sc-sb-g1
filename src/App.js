e339421327fba173ca910c7fb019b5014afb5f19
Gérome Lacaux <gerome.lacaux@hetic.net>
2020-05-22 13:05:38 +0200

    poc pull request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Badge, { colors } from "./ui/atoms/Badge";
-
+// This is a poc of how to pull request
 export default function App() {
-      return (
-            <div className="App">
-                <Badge text="IRL" color={colors.purple}/>
-                <Badge text="IRL" color={colors.grey}/>
-            </div>
-      );
+  return (
+    <div className="App">
+      <Badge text="IRL" color={colors.purple} />
+      <Badge text="IRL" color={colors.grey} />
+    </div>
+  );
 }
